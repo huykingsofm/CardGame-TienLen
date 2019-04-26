723f0612d7e6ae5f812b57c3925cb88e62079d5c
anvanhieu <vanhieuan99@gmail.com>
2019-04-26 20:15:45 +0700

    Tien Len v0

---
+++ Server/test/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+   
+
+    public class Class2
+    {
+        public int n1 {
+            get;
+            protected set;
+        }
+        public class Class1
+        {
+            internal protected int n;
+            public Class1()
+            {
+                Class2 c2 = null;
+                c2.n1 = 1;
+            }
+        } 
+        Class1 c;
+        public Class2()
+        {
+            c.n = 0;
+        }
+    }
+}
---
+++ Server/test/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class2.Class1 c = new Class2.Class1();
+            c.n = 1;
+            Class2 c2 = null;
+            int a = c2.n1;
+        }
+    }
+}
